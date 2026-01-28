1c85d5a336a979ff84461b0fb78ed84a5044f6b4
nsjjjjjj <nsj050524@gmail.com>
2026-01-28 18:57:15 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,8 +1,2 @@
 def solution(myString):
-    answer = list(myString)
-    for i in range(len(answer)):
-        if answer[i] == 'a' or answer[i] == 'A':
-            answer[i] = answer[i].upper()
-        else:
-            answer[i] = answer[i].lower()
-    return ''.join(answer)
+    return myString.lower().replace('a','A')
